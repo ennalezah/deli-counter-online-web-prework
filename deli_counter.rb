9b0225fc8bb86419c609cb6b1e8853f47bb412ce
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-05-30 03:54:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,34 @@
-katz_deli = []
+require 'pry' 
 
-def line(katz_deli)
+
+
+# I would like this to still pretty much work the same, but..
+# - no arguments allowed 
+# - we are not using the tests 
+# - instead of a name, when a new customer walks in they just get a number 
+# - the line is currently [ 8734, 8735 ] .. the next number is 8736
+
+KATZ_DELI = [8734, 8735]
+
+def line
   current_line = []
   
-  if katz_deli.length == 0
+  if KATZ_DELI.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each.with_index(1) do |name, index|
+    KATZ_DELI.each.with_index(1) do |name, index|
       current_line << "#{index}. #{name}"
     end
     puts "The line is currently: #{current_line.join(" ")}"
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number
+  KATZ_DELI << name
+  puts "Welcome, #{name}. You are number #{KATZ_DELI.length} in line."
 end
 
-def now_serving(katz_deli)
+def now_serving
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
@@ -26,3 +36,34 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
+
+binding.pry
+
+# katz_deli = []
+
+# def line(katz_deli)
+#   current_line = []
+  
+#   if katz_deli.empty?
+#     puts "The line is currently empty."
+#   else
+#     katz_deli.each.with_index(1) do |name, index|
+#       current_line << "#{index}. #{name}"
+#     end
+#     puts "The line is currently: #{current_line.join(" ")}"
+#   end
+# end
+
+# def take_a_number(katz_deli, name)
+#   katz_deli << name
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
+
+# def now_serving(katz_deli)
+#   if katz_deli.length == 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{katz_deli[0]}."
+#     katz_deli.shift
+#   end
+# end
